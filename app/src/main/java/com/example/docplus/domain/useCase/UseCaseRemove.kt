53c755af5e63b4b61645b39b7dcf88251bd8bbe0
PavelShramko <pavel.shramko.97@mail.ru>
2021-08-09 00:34:43 +0300

    внес правки с даггером сохраняет и удаляет

---
+++ app/src/main/java/com/example/docplus/domain/useCase/UseCaseRemove.kt
@@ -0,0 +1,21 @@
+package com.example.docplus.domain.useCase
+
+import com.example.docplus.domain.DoctorRepository
+import kotlinx.coroutines.*
+import javax.inject.Inject
+
+
+class UseCaseRemove @Inject constructor (
+    private val repository: DoctorRepository
+) {
+
+    val scope = CoroutineScope(Dispatchers.Default + Job())
+
+    fun removeById(id: Long) {
+        scope.launch {
+            withContext(Dispatchers.IO) {
+                repository.removeById(id)
+            }
+        }
+    }
+}
